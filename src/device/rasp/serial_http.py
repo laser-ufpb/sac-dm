b274f6db72d2e66f9e3d4f2b58d4d501c3511558
franciscolimacf <franciscolimacf@gmail.com>
2023-04-19 11:23:30 -0300

    serial_http.py code update

--- src/device/rasp/serial_http.py
+++ src/device/rasp/serial_http.py
@@ -9,9 +9,6 @@ url = 'https://enmpf6xid68v.x.pipedream.net/'
 sensor_buffer = []
 
 while 1:
-    
-    if len(sensor_buffer) == 10:
-        break
 
     esp_serial = ser.readline()
     print(esp_serial)
@@ -19,6 +16,9 @@ while 1:
     sensor_buffer.append(esp_serial)
 
     time.sleep(1)
+
+    if len(sensor_buffer) == 10:
+        break
 
 x = requests.post(url, json={'data_rasp': sensor_buffer})
 
