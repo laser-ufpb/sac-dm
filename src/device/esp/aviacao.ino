ad05badc23e6fa8f53ceb00fdda810f4163e6225
franciscolimacf <franciscolimacf@gmail.com>
2023-04-19 14:32:23 -0300

    mpu6050 sensor readings and http post updated

--- src/device/esp/aviacao.ino
+++ src/device/esp/aviacao.ino
@@ -9,29 +9,15 @@ void setup() {
   wifi.WifiSetup("iPhone de Francisco", "xgdkmomw6");
   accel.setConfig();
   //wifi.HttpRegister();
-
-
-  accel.getEvent();
-
-  delay(50);
-
-  wifi.HttpPostAccel(String(accel.a.acceleration.x), String(accel.a.acceleration.y), String(accel.a.acceleration.z));
-  
-  
 }
 
 void loop() {
-  
-  //Serial.begin(9600);
-    
-  //String data;
+
+  accel.getEvent();
+  wifi.HttpPostAccel(String(accel.a.acceleration.x), String(accel.a.acceleration.y), String(accel.a.acceleration.z));
 
 
-  //data = String(accel.a.acceleration.x) + "," + String(accel.a.acceleration.y) + "," + String(accel.a.acceleration.z);
-  
-  //Serial.println(data);
-
-  //delay(5000);
+  delay(5000);
 
   
 }
