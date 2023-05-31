5620214ab08c70169c094efd94afa4c5ab03149c
RosasGabriel <gabriel.rosas7232@gmail.com>
2023-05-31 15:20:58 -0300

    Comunicação Login front-back, precisa de ajustes

--- client/pudim-app/src/app/app.module.ts
+++ client/pudim-app/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import { LoginService } from './services/login_service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LoginService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/pudim-app/src/app/pages/login/login.page.ts
+++ client/pudim-app/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login_service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  username: string = '';
+  password: string = '';
+  loginError: string | null = null;
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
   }
 
+  login()
+  {
+    this.loginService.login(this.username, this.password).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        this.loginError = error.message;
+        console.log(error);
+      }
+    )
+  }
 }
