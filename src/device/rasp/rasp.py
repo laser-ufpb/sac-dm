6719b5c48d511e8baa08eed27671725da6fc5a1e
Francisco Lima <franciscolimacf@gmail.com>
2023-06-11 22:18:26 -0300

    simplifying code to adapt esp32 code changes

--- src/device/rasp/rasp.py
+++ src/device/rasp/rasp.py
@@ -18,96 +18,64 @@ def check_connection(host='https://google.com/'):
 	except:
 		return False
 		
-#async def make_readings(sensor_buffer):
-#    for r in sensor_buffer:
-#        yield r		
+async def make_readings(sensor_buffer):
+    for r in sensor_buffer:
+        yield r		
 		
-#async def send_http(sbuffer):
-#	url = 'https://enmpf6xid68v.x.pipedream.net/'
-#	async with aiohttp.ClientSession() as session:
-#		post_task=[]
-#		#async for reading in sbuffer:
-#		async for reading in make_readings(sbuffer):
-#			post_task.append(send_post(session, url, reading))
-#		await asyncio.gather(*post_task)
+async def send_http(sbuffer):
+	url = 'https://enmpf6xid68v.x.pipedream.net/'
+	async with aiohttp.ClientSession() as session:
+		post_task=[]
+		async for reading in make_readings(sbuffer):
+			post_task.append(send_post(session, url, reading))
+		await asyncio.gather(*post_task)
 
-#async def send_post(session, url, reading):
-#	reading_split = reading.split(",")
-#	#print(reading_split)
-#	async with session.post(url, data={
-#					"device_code":"01",
-#					"ACx": reading_split[0],
-#					"ACy": reading_split[1],
-#					"ACz": reading_split[2],
-#					"timestamp": reading_split[3]
-#					}) as response:
-#						data = await response.text()
+async def send_post(session, url, reading):
+	reading_split = reading.split(",")
+	#print(reading_split)
+	async with session.post(url, data={
+					"device_code":"01",
+					"ACx": reading_split[0],
+					"ACy": reading_split[1],
+					"ACz": reading_split[2],
+					"timestamp": reading_split[3]
+					}) as response:
+						data = await response.text()
+
+	
+########################### GLOBAL VAR #################################
 
 url = 'https://enmpf6xid68v.x.pipedream.net/'
-def send_http(data,lock):
-	buffer_split = reading.split(",")
-	x = requests.post(url, json={'device_code': '01',
-								"ACx": buffer_split[0],
-								"ACy": buffer_split[1],
-								"ACz": buffer_split[2], 
-								"time": buffer_split[3]})
-		
-############################ TEXT FILES ################################
-		
-def write_log(log, data, lock):
-	for reading in data:
-		with lock:
-			log.write(reading)
-			log.write("\n")
+#url = 'http://192.168.0.108:8000/accelerometer/'
 
-		
-def write_temp(temp, data, lock):
-	for reading in data:
-		with lock:
-			temp.write(reading)
-			temp.write("\n")
-	
-#def read_temp_send():
-
-		
-############################ MAIN CODE #################################
-
-log_lock = Lock()
-temp_lock = Lock()
+online = True
 
 ser = serial.Serial("/dev/ttyS0", 115200)
 ser.reset_input_buffer()
-
 sensor_buffer = []
 
-log = open("log.txt","a")
-temp = open("temp.txt","a")
+############################ MAIN CODE #################################
 
-start = time.time()
 esp_serial = str(ser.readline())
-
 while True:
 	
 	esp_serial = str(ser.readline())
 	sensor_buffer.append(esp_serial[2:][:-5] + ','+ str(time.time()))
 	
-	if len(sensor_buffer) >= 1000:
-	
-		t_log = Thread(target=write_log, args=(log, sensor_buffer, log_lock,))
-		t_log.start()
-		if(check_connection()):
-			x=0
-			#asyncio.run(send_http(sensor_buffer))
-			#t_http = Thread(target=send_http, args=(sensor_buffer))
-			sensor_buffer.clear()
+	if len(sensor_buffer) == 9999:
+		break;
+		
+print(len(sensor_buffer))
+print(sensor_buffer[0])
+
+if(online):
+	asyncio.run(send_http(sensor_buffer))
 				
-		else:
-			t_temp = Thread(target=write_temp, args=(temp, sensor_buffer, temp_lock))
-			t_temp.start()
-			sensor_buffer.clear()
+else:
+	
+	log = open("log.txt", "a")
+	for data in sensor_buffer:
+		log.write(data)
+		log.write("\n")
 			
-	if ((time.time() - start) > 100):
-		print("finishing\n")
-		print(time.time() - start)
-		break
 				
