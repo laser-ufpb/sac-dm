2865eff1a07a314986a0ecc042c169f496f562bf
davigama10 <davigama10@gmail.com>
2023-08-11 12:14:39 -0300

    gets melhores e uso de try except

--- src/server/controllers/accelerometer.py
+++ src/server/controllers/accelerometer.py
@@ -9,9 +9,15 @@ from fastapi.responses import JSONResponse
 
 
 def create_accelerometer_record(accelerometer_schema: List[AccelerometerSchema], db: Session):
-    records = [AccelerometerAcquisition(**accelerometer_record.dict()) for accelerometer_record in accelerometer_schema]
-    db.add_all(records)
-    db.commit()
+    try:
+        records = [AccelerometerAcquisition(**accelerometer_record.dict()) for accelerometer_record in accelerometer_schema]
+        db.add_all(records)
+        db.commit()
+    except Exception:
+        return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"error": "Failed to insert data to the database."}
+    )
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content="Successfully entered data!")
@@ -21,19 +27,20 @@ def get_all_accelerometer_records(db: Session):
 
     
 def get_accelerometer_record_by_device_id(data: Filter, db: Session):
-    return db.query(AccelerometerAcquisition).filter(AccelerometerAcquisition.device_id == data.device_id).all()
+    if data.device_id:
+        return db.query(AccelerometerAcquisition).filter(AccelerometerAcquisition.device_id == data.device_id).all()
 
 
 def get_accelerometer_record_by_datetime(data: Filter, db: Session):
     if data.datetime_initial and not data.datetime_final:
         return db.query(AccelerometerAcquisition).filter(AccelerometerAcquisition.timestamp > data.datetime_initial).all()
-    else:
+    elif data.datetime_initial and data.datetime_final:
         return db.query(AccelerometerAcquisition).filter(AccelerometerAcquisition.timestamp > data.datetime_initial, AccelerometerAcquisition.timestamp < data.datetime_final).all()
     
 
 def get_accelerometer_record_by_device_id_and_datetime(data: Filter, db: Session):
-    if data.datetime_initial and not data.datetime_final:
+    if data.device_id and data.datetime_initial and not data.datetime_final:
         return db.query(AccelerometerAcquisition).filter(AccelerometerAcquisition.device_id == data.device_id, AccelerometerAcquisition.timestamp > data.datetime_initial).all()
-    else:
+    elif data.device_id and data.datetime_initial and data.datetime_final:
         return db.query(AccelerometerAcquisition).filter(AccelerometerAcquisition.device_id == data.device_id, AccelerometerAcquisition.timestamp > data.datetime_initial, AccelerometerAcquisition.timestamp < data.datetime_final).all()
  
--- src/server/controllers/sac_dm.py
+++ src/server/controllers/sac_dm.py
@@ -9,9 +9,15 @@ from fastapi.responses import JSONResponse
 
 
 def create_sacdm(sac_dm_schema: List[SACDMSchema], db: Session):
-    sac_dm_data = [SACDM(**sac_dm.dict()) for sac_dm in sac_dm_schema]
-    db.add_all(sac_dm_data)
-    db.commit()
+    try:
+        sac_dm_data = [SACDM(**sac_dm.dict()) for sac_dm in sac_dm_schema]
+        db.add_all(sac_dm_data)
+        db.commit()
+    except Exception:
+        return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"error": "Failed to insert data to the database."}
+    )
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content="Successfully entered data!")
@@ -22,19 +28,20 @@ def get_all_sacdm(db: Session):
 
     
 def get_sacdm_by_device_id(data: Filter, db: Session):
-    return db.query(SACDM).filter(SACDM.device_id == data.device_id).all()
+    if data.device_id:
+        return db.query(SACDM).filter(SACDM.device_id == data.device_id).all()
 
 
 def get_sacdm_by_datetime(data: Filter, db: Session):
     if data.datetime_initial and not data.datetime_final:
         return db.query(SACDM).filter(SACDM.timestamp > data.datetime_initial).all()
-    else:
+    elif data.datetime_initial and data.datetime_final:
         return db.query(SACDM).filter(SACDM.timestamp > data.datetime_initial, SACDM.timestamp < data.datetime_final ).all()
     
     
 def get_sacdm_by_device_id_and_datetime(data: Filter, db: Session):
-    if data.datetime_initial and not data.datetime_final:
+    if data.device_id and data.datetime_initial and not data.datetime_final:
         return db.query(SACDM).filter(SACDM.device_id == data.device_id, SACDM.timestamp > data.datetime_initial).all()
-    else:
+    elif data.device_id and data.datetime_initial and data.datetime_final:
         return db.query(SACDM).filter(SACDM.device_id == data.device_id, SACDM.timestamp > data.datetime_initial, SACDM.timestamp < data.datetime_final ).all()
     
