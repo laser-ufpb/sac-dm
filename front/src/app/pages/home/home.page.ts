3fbd9060d4883f34a4769856d9bcd5e302c65727
RosasGabriel <gabriel.rosas7232@gmail.com>
2023-08-28 11:07:54 -0300

    Redirecionamento Home para Login

--- front/src/app/pages/home/home.page.ts
+++ front/src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { LoginPage } from '../login/login.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public navCtrl: NavController) {}
 
+  redirect_login()
+  {
+    this.navCtrl.navigateRoot('login');
+  }
 }
