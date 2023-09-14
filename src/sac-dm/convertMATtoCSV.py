dc5faa05e5750361a32cf15823b4447072230c83
NthnR <nathan.rtl.pb@gmail.com>
2023-09-14 13:45:48 -0300

    Convertendo arquivos.

--- src/sac-dm/convertMATtoCSV.py
+++ src/sac-dm/convertMATtoCSV.py
@@ -1,53 +1,41 @@
 import scipy.io
 import numpy as np
 
-file_path = "../../files/hexacopter_signals/mat/nominal_flight/NFlt01.mat"
+file_path = "../../files/hexacopter_signals/mat/nominal_flight/NFlt05.mat"
 
+file_name = "../../files/hexacopter_signals/csv/nominal_flight/NFlt05"
 file = scipy.io.loadmat(file_path)
 
-# file['n1'][0][0][0] - lista time vector
-# file['n1'][0][0][1][0] - lista eixo x
-# file['n1'][0][0][2][0] - lista eixo y
-# file['n1'][0][0][3][0] - lista eixo z
-# print(len(file['n1'][0][0][0] ))
-# print(len(file['n1'][0][0][1][0]  ))
-# print(len(file['n1'][0][0][2][0]  ))
-# print(len(file['n1'][0][0][3][0]  ))
 
+print(f"Tamanhos[n1] - Eixo X: {len(file['n1'][0][0][1][0])} Eixo Y: {len(file['n1'][0][0][2][0])} Eixo Z: {len(file['n1'][0][0][3][0]  )} Time: {len(file['n1'][0][0][0] )}")
 
-# file_csv = open("../../files/hexacopter_signals/nominal_flight/NFlt01-n1.csv", "w")
+file_csv = open( (file_name + "n1.csv"), "w")
 
-# for i in range(len(file['n1'][0][0][0])):
-#     file_csv.write(f"{file['n1'][0][0][1][0][i]} ; {file['n1'][0][0][2][0][i]} ; {file['n1'][0][0][3][0][i]} ; {file['n1'][0][0][0][i][0]}")
-#     if(i < (len(file['n1'][0][0][0]) - 1)):
-#         file_csv.write("\n")
+for i in range(len(file['n1'][0][0][0])):
+    file_csv.write(f"{file['n1'][0][0][1][0][i]} ; {file['n1'][0][0][2][0][i]} ; {file['n1'][0][0][3][0][i]} ; {file['n1'][0][0][0][i][0]}")
+    if(i < (len(file['n1'][0][0][0]) - 1)):
+        file_csv.write("\n")
 
-# file_csv.close()
+file_csv.close()
 
-# print(len(file['n2'][0][0][0] ))
-# print(len(file['n2'][0][0][1][0]  ))
-# print(len(file['n2'][0][0][2][0]  ))
-# print(len(file['n2'][0][0][3][0]  ))
+print(f"Tamanhos[n2] - Eixo X: {len(file['n2'][0][0][1][0])} Eixo Y: {len(file['n2'][0][0][2][0])} Eixo Z: {len(file['n2'][0][0][3][0]  )} Time: {len(file['n2'][0][0][0] )}")
 
-# file_csv = open("../../files/hexacopter_signals/nominal_flight/NFlt01-n2.csv", "w")
+file_csv = open((file_name + "n2.csv"), "w")
 
-# for i in range(len(file['n2'][0][0][0])):
-#     file_csv.write(f"{file['n2'][0][0][1][0][i]} ; {file['n2'][0][0][2][0][i]} ; {file['n2'][0][0][3][0][i]} ; {file['n2'][0][0][0][i][0]}")
-#     if(i < (len(file['n2'][0][0][0]) - 1)):
-#         file_csv.write("\n")
+for i in range(len(file['n2'][0][0][0])):
+    file_csv.write(f"{file['n2'][0][0][1][0][i]} ; {file['n2'][0][0][2][0][i]} ; {file['n2'][0][0][3][0][i]} ; {file['n2'][0][0][0][i][0]}")
+    if(i < (len(file['n2'][0][0][0]) - 1)):
+        file_csv.write("\n")
 
-# file_csv.close()
+file_csv.close()
 
-print(len(file['n3'][0][0][0][0] ))
-print((file['n3'][0][0][1] ))
-# print(len(file['n3'][0][0][2][0]  ))
-# print(len(file['n3'][0][0][3][0]  ))
+print(f"Tamanhos[n3] - Eixo X: {len(file['n3'][0][0][4][0])} Eixo Y: {len(file['n3'][0][0][5][0])} Eixo Z: {len(file['n3'][0][0][6][0])} Time: {len(file['n3'][0][0][2])}")
 
-# file_csv = open("../../files/hexacopter_signals/nominal_flight/NFlt01-n3.csv", "w")
+file_csv = open((file_name + "n3.csv"), "w")
 
-# for i in range(len(file['n3'][0][0][0])):
-#     file_csv.write(f"{file['n3'][0][0][1][0][i]} ; {file['n3'][0][0][2][0][i]} ; {file['n3'][0][0][3][0][i]} ; {file['n3'][0][0][0][i][0]}")
-#     if(i < (len(file['n3'][0][0][0]) - 1)):
-#         file_csv.write("\n")
+for i in range(len(file['n3'][0][0][4][0])):
+    file_csv.write(f"{file['n3'][0][0][4][0][i]} ; {file['n3'][0][0][5][0][i]} ; {file['n3'][0][0][6][0][i]} ; {file['n3'][0][0][2][i]}")
+    if(i < (len(file['n3'][0][0][0]) - 1)):
+        file_csv.write("\n")
 
-# file_csv.close()
+file_csv.close()
