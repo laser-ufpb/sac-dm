05b965923179527626712763b06bbbd5bc278c9a
Francisco Lima <franciscolimacf@gmail.com>
2023-10-03 13:43:23 -0300

    converting raw data to angle

--- src/device/rasp/rasp.py
+++ src/device/rasp/rasp.py
@@ -7,7 +7,12 @@ import json as JSON
 from threading import Thread
 from threading import Lock
 
-############################ SEND HTTP ################################# 
+############################ SEND HTTP #################################
+
+def convertData(d):
+	fdata = float(d)
+	converted = ((fdata)*9.80665)/4096
+	return round(converted, 2) 
 
 
 
@@ -19,30 +24,49 @@ def check_connection(host='https://google.com/'):
 		return False
 
 def sendJSONhttp(data, lock):
-	print("prepping json")
+	#print("prepping json")
 	json_obj_array = []
 	for r in data:
 		split = r.split(";")
 		item = {'device_id':'01',
-				'ACx': split[0],
-				'ACy': split[1],
-				'ACz': split[2],
+				'ACx': convertData(split[0]),
+				'ACy': convertData(split[1]),
+				'ACz': convertData(split[2]),
 				'timestamp': split[3]}
 		json_obj_array.append(item)
 		
 	json_string = JSON.dumps(json_obj_array)
 	with lock:
 		response = requests.post(url, data=json_string, headers={"Content-Type":"application/json"})
-		print(response.text)
+		print(response.status_code)
+		return response.status_code
 
 ############################ TEXT FILES ################################
 		
-def write_log(log, data, lock):
+def write_log(data, lock):
+	log = open("log.txt", "a")
 	for reading in data:
 		with lock:
 			log.write(reading)
 			log.write("\n")
+	log.close()
 
+def read_log_send(lock)
+	sendHttp = []
+
+	with lock:
+		log = open("log.txt", "r")
+		lines = log.readlines()
+		log.close()
+		for l in lines:
+			sendHttp.append(l.strip())
+		if(sendJSONhttp(sendHttp, http_lock) == 200):
+			log = open("log.txt", "w")
+			log.close()
+			sendHttp.clear()
+	
+		
+		
 	
 ########################### GLOBAL VAR #################################
 
@@ -55,7 +79,7 @@ ser = serial.Serial("/dev/ttyS0", 115200)
 ser.reset_input_buffer()
 sensor_buffer = []
 
-log = open("log.txt", "a")
+disconnected_flag = 0 # 1 if previously disconnected then reconnect to wifi, 0 if not
 
 log_lock = Lock()
 http_lock = Lock()
@@ -70,7 +94,7 @@ while True:
 	start=time.time()	
 	
 	esp_serial = str(ser.readline())
-	sensor_buffer.append(esp_serial[2:][:-5] + ';'+ str(time.time()))
+	sensor_buffer.append(esp_serial[2:][:-5] + ';'+ str(time.time_ns()*1000000))
 	
 	if len(sensor_buffer) %10000 == 0:
 		print(sensor_buffer[-1])
@@ -87,6 +111,9 @@ while True:
 
 		if(check_connection()):
 			
+			if(disconnected_flag):
+				read_log_send()
+			
 			print("online")
 			t_http = Thread(target=sendJSONhttp, args=(toSend, http_lock))
 			t_http.start()
@@ -95,8 +122,9 @@ while True:
 					
 		else:
 			
+			disconnected_flag = 1
 			print("offline")
-			t_log = Thread(target=write_log, args=(log, toSend, log_lock,))
+			t_log = Thread(target=write_log, args=(toSend, log_lock,))
 			t_log.start()
 			sensor_buffer.clear()
 			
