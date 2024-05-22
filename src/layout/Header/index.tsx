a3fdc239b8c30187f1738edcaf2167517f95f470
João Victor Pedrosa <joao.victor@controlit.com.br>
2024-05-22 09:19:22 -0300

    Remover lógica desnecessária de redimensionamento de tela no Header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,65 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import { isMenuItemActive } from "../../app/utils/isMenuItemActive";
 import { Container, NavigationButton } from "./styles";
-import { useEffect, useState } from "react";
 import { Devices, Equalizer, Person } from "@mui/icons-material";
 
 export const Header = () => {
-  const [isDesktop, setIsDesktop] = useState(() => {
-    return window.innerWidth > 768 ? true : false;
-  });
-
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setIsDesktop(true);
-      } else {
-        setIsDesktop(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
   return (
     <Container>
-      {isDesktop ? (
-        <>
-          {/* <NavigationButton
+      {/* <NavigationButton
             isActive={isMenuItemActive("/dashboard")}
             onClick={() => navigate("/dashboard")}
           >
             <Dashboard />
           </NavigationButton> */}
-          <NavigationButton
-            isActive={isMenuItemActive("/devices")}
-            onClick={() => navigate("/devices")}
-          >
-            <Devices />
-          </NavigationButton>
-          <NavigationButton
-            isActive={isMenuItemActive("/sac_dm")}
-            onClick={() => navigate("/sac_dm")}
-          >
-            <Equalizer />
-          </NavigationButton>
+      <NavigationButton
+        isActive={isMenuItemActive("/devices")}
+        onClick={() => navigate("/devices")}
+      >
+        <Devices />
+      </NavigationButton>
+      <NavigationButton
+        isActive={isMenuItemActive("/sac_dm")}
+        onClick={() => navigate("/sac_dm")}
+      >
+        <Equalizer />
+      </NavigationButton>
 
-          <NavigationButton
-            isActive={isMenuItemActive("/account")}
-            onClick={() => navigate("/account")}
-            style={{
-              position: "absolute",
-              right: "8px",
-            }}
-          >
-            <Person />
-          </NavigationButton>
-        </>
-      ) : (
-        <div className="menu-mobile">MOBILE</div>
-      )}
+      <NavigationButton
+        isActive={isMenuItemActive("/account")}
+        onClick={() => navigate("/account")}
+        style={{
+          position: "absolute",
+          right: "8px",
+        }}
+      >
+        <Person />
+      </NavigationButton>
     </Container>
   );
 };
