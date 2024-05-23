9ac70aae29cbc68aa9001a0f8cbd4a4a6090eb85
Joao Victor <joao.victor@controlit.com.br>
2024-05-23 10:54:21 -0300

    fix: Update EmptyData component styles

--- src/components/EmptyData/styles.ts
+++ src/components/EmptyData/styles.ts
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 300px;
-  width: 100%;
-  opacity: 0.5;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  padding: 32px;
+  transform: translate(-50%, -50%);
+  width: max-content;
+  border-radius: 8px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
+  background-color: ${({ theme }) => theme.gray800};
 
   p {
     font-size: 20px;
