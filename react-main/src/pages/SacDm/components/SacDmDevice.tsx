a977c85f34c35840afe4f36be8b2ec734466840f
Joao Victor Pedrosa <jotavpedrosa@gmail.com>
2024-06-03 12:25:33 -0300

    feat: media e desvio padrao no grafico sadm

--- react-main/src/pages/SacDm/components/SacDmDevice.tsx
+++ react-main/src/pages/SacDm/components/SacDmDevice.tsx
@@ -13,6 +13,18 @@ export const SacDmDevice = ({
     return null;
   }
 
+  // Calcular média e desvio padrão
+  const values = sacDm.map((item: SacDmProps) => +item.value.toFixed(8));
+  const mean = values.reduce((acc, value) => acc + value, 0) / values.length;
+  const stdDev = Math.sqrt(
+    values.reduce((acc, value) => acc + (value - mean) ** 2, 0) / values.length
+  );
+
+  // Criar séries para média e desvio padrão
+  const meanSeries = Array(values.length).fill(mean);
+  const stdDevUpper = meanSeries.map((m) => m + stdDev);
+  const stdDevLower = meanSeries.map((m) => m - stdDev);
+
   const optionsChart = {
     chart: {
       id: "device-metrics",
@@ -23,15 +35,35 @@ export const SacDmDevice = ({
         show: false,
       },
     },
+    yaxis: {
+      labels: {
+        formatter: (value: number) => value.toFixed(8), // Formatando labels do eixo y para mostrar 8 casas decimais
+      },
+    },
     tooltip: {
       theme: "dark",
+      y: {
+        formatter: (value: number) => value.toFixed(8), // Formatando valores do tooltip para mostrar 8 casas decimais
+      },
     },
   };
 
   const seriesChart = [
     {
       name: "Valor",
-      data: sacDm.map((item: SacDmProps) => +item.value.toFixed(8)),
+      data: values,
+    },
+    {
+      name: "Média",
+      data: meanSeries.map((value) => parseFloat(value.toFixed(8))), // Garantindo a precisão de 8 casas decimais
+    },
+    {
+      name: "Desvio Padrão Superior",
+      data: stdDevUpper.map((value) => parseFloat(value.toFixed(8))),
+    },
+    {
+      name: "Desvio Padrão Inferior",
+      data: stdDevLower.map((value) => parseFloat(value.toFixed(8))),
     },
   ];
 
@@ -41,7 +73,7 @@ export const SacDmDevice = ({
         options={optionsChart}
         series={seriesChart}
         type="line"
-        height={350}
+        height="350"
       />
       {sacDm.length === 0 && (
         <EmptyData message="Nenhum dado encontrado para o dispositivo selecionado" />
