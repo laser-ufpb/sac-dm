f38984d7b26d80b05e810af862c7c1fc77921f3a
Francisco Lima <42159107+franciscolimacf@users.noreply.github.com>
2024-06-10 10:19:07 -0300

    Delete unused ESP32 code

--- src/device/esp/ESP32_MPU_or_ADXL/ESP32_MPU_or_ADXL.ino
+++ src/device/esp/ESP32_MPU_or_ADXL/ESP32_MPU_or_ADXL.ino
@@ -1,44 +1,47 @@
-#include <ESP32Servo.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_MPU6050.h>
 #include <Adafruit_ADXL345_U.h>
-#include "WiFi.h"
-#include "AsyncUDP.h"
-#include "ESC.h"
-
-volatile int line = 0;
-
-// Objeto do acelerômetro
-// O parâmetro é um identificador exclusivo, pode ser qualquer número
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345); 
-
-// Mutex para garantir acesso exclusivo às variáveis compartilhadas
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-
-/* Objeto do servo Motor */
-Servo ServoMotor;
-
+#include <ESP32Servo.h>
 
 #define ESCPin 17
 #define USE_SERIAL Serial
 
-
+Servo ServoMotor;
 int potServor=0;
+ 
+String acc = "";
+ 
+volatile int line = 0;
+ 
+// Objetos do acelerômetro
+Adafruit_MPU6050 accelMPU;
+Adafruit_ADXL345_Unified accelADXL = Adafruit_ADXL345_Unified(12345);
+ 
+// Mutex para garantir acesso exclusivo às variáveis compartilhadas
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+ 
+/* Objeto do servo Motor */
+
 
 // Variáveis compartilhadas entre os núcleos
 volatile float accelDataX = 0.0;
 volatile float accelDataY = 0.0;
 volatile float accelDataZ = 0.0;
-
+ 
 void task1(void *pvParameters) {
   (void)pvParameters;
-
-  sensors_event_t event;
-
+ 
+  sensors_event_t event, g, temp;
+ 
   while (1) {
     // Leitura dos dados do acelerômetro
-    accel.getEvent(&event);
-
+    if(acc == "MPU"){
+      accelMPU.getEvent(&event, &g, &temp);
+    } else{
+      accelADXL.getEvent(&event);
+    }
+ 
     // Aquisição do mutex para garantir acesso exclusivo às variáveis compartilhadas
     portENTER_CRITICAL(&mux);
     accelDataX = event.acceleration.x;
@@ -50,46 +53,70 @@ void task1(void *pvParameters) {
       potServor = USE_SERIAL.parseInt();
       ServoMotor.write(potServor);
     }
-
+ 
   }
 }
-
+ 
 void task2(void *pvParameters) {
   (void)pvParameters;
   char cMsg[254];
   line++;
-
+  int delay = 320;
+ 
+  if(acc == "ADXL"){
+    delay = 160;
+  } 
+ 
   while (1) {
     // Aquisição do mutex para garantir acesso exclusivo às variáveis compartilhadas
     portENTER_CRITICAL(&mux);
     float x = accelDataX;
     float y = accelDataY;
     float z = accelDataZ;
+ 
     portEXIT_CRITICAL(&mux);
-
-    //sprintf(cMsg, "%0.2f;%0.2f;%0.2f;%d;%d", x, y, z, millis(), line++ );
-    sprintf(cMsg, "%0.2f;%0.2f;%0.2f", x, y, z);
+ 
+    sprintf(cMsg, "%0.2f;%0.2f;%0.2f", x, y, z );
+    //sprintf(cMsg, "%0.2f;%0.2f;%0.2f", x, y, z);
     Serial.println(cMsg);
-    delayMicroseconds(160);
+    delayMicroseconds(delay);
   }
 }
-
+ 
+ 
 void setup() {
   Serial.begin(921600);
-  //Serial1.begin(115200);  
-
+  //Serial.begin(230400);  
+  delay(1000);
   // Inicialização do acelerômetro
-  if (!accel.begin()) {
-    while (1){
-      Serial.println("Falha ao iniciar o ADXL345!");
-      delay(1000);
+  Serial.println("Testando inicio");
+ 
+    if (!accelMPU.begin()) {
+      Serial.println("Falha ao iniciar o MPU6050!");
+ 
+      if (!accelADXL.begin()) {
+        Serial.println("Falha ao iniciar o ADXL345!");
+        while (1){
+          Serial.println("Falha ao iniciar os dois!");
+          delay(1000);
+        }
+      }
+      else {
+        acc = "ADXL";
+      }
+ 
+    } else {
+      acc = "MPU";
     }
-  }
-
-
   delay(500); 
-  accel.setRange(ADXL345_RANGE_2_G);
-  accel.setDataRate(ADXL345_DATARATE_1600_HZ);
+  if(acc == "MPU"){
+    accelMPU.setAccelerometerRange(MPU6050_RANGE_2_G);
+    accelMPU.setGyroRange(MPU6050_RANGE_500_DEG);
+    accelMPU.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  } else { 
+    accelADXL.setRange(ADXL345_RANGE_2_G);
+    accelADXL.setDataRate(ADXL345_DATARATE_1600_HZ);
+  }
   delay(500);
 
   //  Servor 
@@ -100,7 +127,7 @@ void setup() {
     ServoMotor.write(0); 
     USE_SERIAL.println("Aguardando potência:");        
   }
- 
+  
   potServor = USE_SERIAL.parseInt();
 
   USE_SERIAL.println("Turning engine on ...");
@@ -108,12 +135,13 @@ void setup() {
   ServoMotor.write(20); delay(2000);
   ServoMotor.write(0);  delay(5000); 
   ServoMotor.write(potServor);
+
  
   // Criação das tasks
   xTaskCreatePinnedToCore(task1, "Task1", 10000, NULL, 1, NULL, 0); // Task 1 no núcleo 0
   xTaskCreatePinnedToCore(task2, "Task2", 10000, NULL, 1, NULL, 1); // Task 2 no núcleo 1
 }
-
+ 
 void loop() {
   // O loop principal é deixado vazio, já que as tasks estão sendo executadas nos núcleos separados
 }
